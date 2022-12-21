8990d76f9937dda4a39ef1c72670116fc82ea65d
Tim McKee <tmckee-contractor@endeavorco.com>
2022-12-21 03:00:05 -0500

    update Readme and formatting

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # MyProducts
-react-native redux rtk query
+## react-native redux rtk query 
+https://www.youtube.com/watch?v=-8WEd578fFw
+
+# Back End API
+
+```bash
+https://dummyjson.com/products
+```
+
+## clone the repo
+
+```bash
+ git clone git@github.com:gatortim50/MyProducts.git
+```
+
+# steps to build and run
+
+```bash
+ yarn install && npx pod-install && yarn ios
+```
+
+Press the button, the products are shown in console.log
+
+itional packages added
+
+```bash
+ yarn add react-redux @reduxjs/toolkit
+```
--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,10 @@
-import { configureStore, createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { productsApi } from './features/apiSlice';
+import {configureStore, createSlice, PayloadAction} from '@reduxjs/toolkit'
+import {productsApi} from './features/apiSlice'
 
 export const store = configureStore({
   reducer: {
     [productsApi.reducerPath]: productsApi.reducer,
-  }, 
-  middleware: (getDefaultMiddleware) =>
-  getDefaultMiddleware().concat(productsApi.middleware),
-});
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(productsApi.middleware),
+})
